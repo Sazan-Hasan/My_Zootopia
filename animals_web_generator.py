269fbbf7e2f544d712c4bf65ee021bae12c92c2e
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-20 16:51:05 +0100

    Make it Nice

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,55 +6,43 @@ def load_data(file_path):
         return json.load(f)
 
 
-def get_animal_lines(animal):
-    lines = []
-
-
-    name = animal.get("name")
-    if name:
-        lines.append(f"Name: {name}")
-
-
-    characteristics = animal.get("characteristics", {})
-    diet = characteristics.get("diet")
-    if diet:
-        lines.append(f"Diet: {diet}")
-
-
-    locations = animal.get("locations", [])
-    if locations:
-        lines.append(f"Location: {locations[0]}")
-
-
-    animal_type = characteristics.get("type")
-    if animal_type:
-        lines.append(f"Type: {animal_type}")
-
-    return lines
-
-
 def generate_animals_string(data):
     output = ""
+
     for animal in data:
-        for line in get_animal_lines(animal):
-            output += line + "\n"
-        output += "\n"
+        output += '<li class="cards__item">\n'
+
+        name = animal.get("name")
+        if name:
+            output += f"Name: {name}<br/>\n"
+
+        characteristics = animal.get("characteristics", {})
+
+        diet = characteristics.get("diet")
+        if diet:
+            output += f"Diet: {diet}<br/>\n"
+
+        locations = animal.get("locations", [])
+        if locations:
+            output += f"Location: {locations[0]}<br/>\n"
+
+        animal_type = characteristics.get("type")
+        if animal_type:
+            output += f"Type: {animal_type}<br/>\n"
+
+        output += "</li>\n"
+
     return output
 
 
 def main():
     data = load_data("animals_data.json")
 
-
     with open("animals_template.html", "r", encoding="utf-8") as f:
         template = f.read()
 
-
     animals_info = generate_animals_string(data)
-
-
     new_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
-
 
     with open("animals.html", "w", encoding="utf-8") as f:
         f.write(new_html)
