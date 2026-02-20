075f660e3b1d5b1d42b3370e2fcb885e43ca3e1c
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-20 16:04:31 +0100

    create a function to read the json data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,6 +1,17 @@
 import json
 
-with open("animals_data.json", "r") as file:
-    data = json.load(file)
 
-print(data)
+def load_animals_data():
+    """Load a json file"""
+    with open("animals_data.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+def main():
+    animals = load_animals_data()
+    print(animals)
+
+
+if __name__ == "__main__":
+    main()
