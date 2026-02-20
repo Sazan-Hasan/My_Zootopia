52ef69d68d99fad5884497298a9e9e1093212c58
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-20 17:07:31 +0100

    Formatting

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,54 +2,56 @@ import json
 
 
 def load_data(file_path):
+    """Load animal data from a JSON file."""
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def generate_animals_string(data):
+def serialize_animal(animal_obj):
+    """Serialize a single animal into an HTML list item."""
+    name = animal_obj.get("name", "")
+
+    characteristics = animal_obj.get("characteristics", {})
+    diet = characteristics.get("diet")
+    animal_type = characteristics.get("type")
+
+    locations = animal_obj.get("locations", [])
+    location = locations[0] if locations else None
+
     output = ""
+    output += '<li class="cards__item">\n'
+    output += f'  <div class="card__title">{name}</div>\n'
+    output += '  <p class="card__text">\n'
 
-    for animal in data:
-        name = animal.get("name", "")
+    if diet:
+        output += f'    <strong>Diet:</strong> {diet}<br/>\n'
+    if location:
+        output += f'    <strong>Location:</strong> {location}<br/>\n'
+    if animal_type:
+        output += f'    <strong>Type:</strong> {animal_type}<br/>\n'
 
-        characteristics = animal.get("characteristics", {})
-        diet = characteristics.get("diet")
-        animal_type = characteristics.get("type")
-
-        locations = animal.get("locations", [])
-        location = locations[0] if locations else None
-
-        output += '<li class="cards__item">\n'
-        output += f'  <div class="card__title">{name}</div>\n'
-        output += '  <p class="card__text">\n'
-
-        if diet:
-            output += f'    <strong>Diet:</strong> {diet}<br/>\n'
-        if location:
-            output += f'    <strong>Location:</strong> {location}<br/>\n'
-        if animal_type:
-            output += f'    <strong>Type:</strong> {animal_type}<br/>\n'
-
-        output += '  </p>\n'
-        output += '</li>\n'
+    output += '  </p>\n'
+    output += '</li>\n'
 
     return output
 
 
+def generate_animals_string(data):
+    """Generate HTML string for all animals."""
+    output = ""
+    for animal in data:
+        output += serialize_animal(animal)
+    return output
+
+
 def main():
-
     data = load_data("animals_data.json")
-
 
     with open("animals_template.html", "r", encoding="utf-8") as f:
         template = f.read()
 
-
     animals_info = generate_animals_string(data)
-
-
     new_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
-
 
     with open("animals.html", "w", encoding="utf-8") as f:
         f.write(new_html)
