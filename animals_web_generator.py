c661e51ab3357fce40da78349588ca3a43d7e52a
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-20 16:19:28 +0100

    create more functions to organize the data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,16 +1,30 @@
 import json
 
+def load_data(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
 
-def load_animals_data():
-    """Load a json file"""
-    with open("animals_data.json", "r") as file:
-        data = json.load(file)
-    return data
+
+def animals_organize(animals):
+    for animal in animals:
+        if "name" in animal:
+            print(f"Name: {animal['name']}")
+
+        if "diet" in animal:
+            print(f"Diet: {animal['diet']}")
+
+        if "locations" in animal and animal["locations"]:
+            print(f"Location: {animal['locations'][0]}")
+
+        if "type" in animal:
+            print(f"Type: {animal['type']}")
+
+        print()
 
 
 def main():
-    animals = load_animals_data()
-    print(animals)
+    animals = load_data("animals_data.json")
+    animals_organize(animals)
 
 
 if __name__ == "__main__":
